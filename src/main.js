15c14aa265b7b6fbb50951142efd4ec6a1068ac9
wuyanzu <1209535419@qq.com>
2019-07-09 16:30:26 +0800

    initial

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 职责1: 导入项目需要依赖的资源 包含 Css...
+// 职责2: 初始化根实例{new Vue() 选项render是把某一个App组件渲染到你指定的容器(.$mount('#app'))内 }
 new Vue({
   render: h => h(App)
 }).$mount('#app')
