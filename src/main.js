38acd7967e0a27d3088b422e951caa41264bb8d2
wuyanzu <1209535419@qq.com>
2019-07-09 17:51:38 +0800

    配置了element-ui

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(Element)
+
 Vue.config.productionTip = false
 
-// 职责1: 导入项目需要依赖的资源 包含 Css...
-// 职责2: 初始化根实例{new Vue() 选项render是把某一个App组件渲染到你指定的容器(.$mount('#app'))内 }
 new Vue({
   render: h => h(App)
 }).$mount('#app')
