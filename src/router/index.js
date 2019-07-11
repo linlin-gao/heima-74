2926067af4e1b1945c23d87beaddc8a3ca20043f
wuyanzu <1209535419@qq.com>
2019-07-11 17:35:04 +0800

    home

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/views/login'
+import home from '@/views/home'
 Vue.use(VueRouter)
 const router = new VueRouter({
   routes: [
-    { name: 'login', path: '/login', component: Login }
+    { path: '/login', redirect: { name: 'login' } },
+    { name: 'login', path: '/login', component: Login },
+    { name: '/', path: '/', commponent: home }
   ]
 })
 
