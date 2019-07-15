a0ca5b6f0be90146faf406057b55b3915cab21ce
wuyanzu <1209535419@qq.com>
2019-07-15 16:33:42 +0800

    渲染完成

--- src/views/artice/index.vue
+++ src/views/artice/index.vue
@@ -40,7 +40,42 @@
             </el-form>
         </el-card>
         <!-- 结果区域 -->
-        <el-card></el-card>
+        <el-card>
+            <div slot="header">
+                根据筛选条件共查询到 <b>0</b>条结果：
+                <el-table :data="articles">
+                    <el-table-column label="封面">
+                        <template slot-scope="scope">
+                            <el-image :src="scope.row.cover.images[0]" style="width:100px;height:75px" alt="">
+                                <div slot="error" class="image-slot">
+                                    <img src="../../assets/images/error.gif" width="100" height="75" alt="">
+                                </div>
+                            </el-image>
+                        </template>
+                    </el-table-column>
+                    <el-table-column label="标题" prop="title"></el-table-column>
+                    <el-table-column label="状态">
+                        <template slot-scope="scope">
+                            <el-tag v-if="scope.row.status === 0" type="info">草稿</el-tag>
+                            <el-tag v-if="scope.row.status === 1" >待审核</el-tag>
+                            <el-tag v-if="scope.row.status === 2" type="success">审核通过</el-tag>
+                            <el-tag v-if="scope.row.status === 3" type="warning">审核失败</el-tag>
+                            <el-tag v-if="scope.row.status === 4" type="danger">已删除</el-tag>
+                        </template>
+                    </el-table-column>
+                    <el-table-column label="发布时间"></el-table-column>
+                    <el-table-column label="操作" width="120">
+                        <template>
+                            <el-button icon="el-icon-edit" plain type="primary" circle></el-button>
+                            <el-button icon="el-icon-delete" plain type="danger" circle></el-button>
+                        </template>
+                    </el-table-column>
+                </el-table>
+                <div class="box">
+                    <el-pagination background layout="prev, pager, next" :total="1000"></el-pagination>
+                </div>
+            </div>
+        </el-card>
     </div>
 </template>
 
@@ -61,7 +96,31 @@ export default {
       // 频道的选项数组
       channelOptions: [{ name: 'java', id: 1 }],
       // 日期数据
-      dateValues: []
+      dateValues: [],
+      // 文章列表数据
+      articles: []
+    }
+  },
+  created () {
+    // 获取频道数据
+    this.getChannelOptions()
+    // 获取文章列表数据
+    this.getArticles()
+  },
+  methods: {
+    // 获取频道数据
+    async getChannelOptions () {
+      // res ===> {data: 响应内容} ===> {data: {data: {channels:[{id,name},...]}}}
+      // 解构赋值 const {data} = res
+      // 解构赋值 const { data: {data} } = res
+      const { data: { data } } = await this.$http.get('channels')
+      this.channelOptions = data.channels
+    },
+    async getArticles () {
+      // post 传参 :post('url', {参数对象})
+      // get 传参 :get('url?key=value&....') / get('url', {params:{参数对象}})
+      const { data: { data } } = await this.$http.get('articles', { params: this.resParams })
+      this.articles = data.results
     }
   }
 }
@@ -71,5 +130,9 @@ export default {
     // element-ui提供的组件 在解析完毕后 会在当前标签上加上一个和组件的名称一致的class
     .el-card{
         margin-bottom: 20px;
+    }
+    .box{
+        text-align: center;
+        margin-top: 20px;
     }
 </style>
