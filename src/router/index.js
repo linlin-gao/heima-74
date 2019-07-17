68e0d980ca235d4d662758f6ec5bd8d43fb9dc07
wuyanzu <1209535419@qq.com>
2019-07-17 19:48:45 +0800

    素材管理完成

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,8 @@ import Home from '@/views/home'
 import Welcome from '@/views/welcome'
 import Article from '@/views/article'
 import NotFound from '@/views/404'
+import Image from '@/views/image'
+import Publish from '@/views/publish'
 
 Vue.use(VueRouter)
 
@@ -17,7 +19,9 @@ const router = new VueRouter({
       component: Home,
       children: [
         { name: 'welcome', path: '/', component: Welcome },
-        { name: 'article', path: '/article', component: Article }
+        { name: 'article', path: '/article', component: Article },
+        { name: 'iamge', path: '/image', component: Image },
+        { name: 'publish', path: '/publish', component: Publish }
       ]
     },
     // ... 很多规则
